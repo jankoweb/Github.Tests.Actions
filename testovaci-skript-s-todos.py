2209a0bfcf4d9a937614de72cbc55d7705bbaeba
Janko <kohojan.work@gmail.com>
2024-11-08 19:10:48 +0100

    Update testovaci-skript-s-todos.py

--- testovaci-skript-s-todos.py
+++ testovaci-skript-s-todos.py
@@ -1,37 +1,37 @@
 import datetime
 
 def get_current_date():
-    """Vrátí aktuální datum ve formátu YYYY-MM-DD"""
+    """Returns the current date in YYYY-MM-DD format"""
     return datetime.datetime.now().strftime("%Y-%m-%d")
 
 def greet_user(username):
-    """Pozdrav uživatele"""
-    # TODO: Přidat personalizovaný pozdrav podle denní doby (např. dobré ráno, dobré odpoledne, dobrý večer)
-    print(f"Ahoj, {username}! Dnes je {get_current_date()}.")
+    """Greets the user"""
+    # TODO: Add a personalized greeting based on the time of day (e.g., good morning, good afternoon, good evening)
+    print(f"Hello, {username}! Today is {get_current_date()}.")
 
 def calculate_age(year_of_birth):
-    """Spočítá věk uživatele na základě roku narození"""
+    """Calculates the user's age based on their birth year"""
     current_year = datetime.datetime.now().year
     age = current_year - year_of_birth
-    # FIXME: Ověřit, jestli je věk správný pro někoho, kdo se narodil ještě letos a neměl narozeniny
+    # FIXME: Verify that the age is correct for someone born this year who hasn’t had their birthday yet
     return age
 
 def display_user_info(username, year_of_birth):
-    """Zobrazí informace o uživateli"""
+    """Displays user information"""
     greet_user(username)
     age = calculate_age(year_of_birth)
-    print(f"{username} má {age} let.")
+    print(f"{username} is {age} years old.")
 
-# Hlavní funkce programu
+# Main program function
 def main():
     username = "Pepa"
     year_of_birth = 2000
 
-    # TODO: Přidat vstup pro uživatele, aby zadal své jméno a rok narození
+    # TODO: Add user input for entering their name and year of birth
     display_user_info(username, year_of_birth)
 
-    # FIXME: Pokud uživatel zadá neplatný rok, zobrazit chybovou zprávu
-    # TODO: Implementovat kontrolu platnosti vstupu
+    # FIXME: If the user enters an invalid year, display an error message
+    # TODO: Implement input validation
 
 if __name__ == "__main__":
     main()
